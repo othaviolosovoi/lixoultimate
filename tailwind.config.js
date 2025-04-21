c72d7ee7f02d9fbba38bdcfc9e34db74f4fb85af
RivianSoldier <bilzims.sg@gmail.com>
2025-04-21 13:17:40 -0300

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        accent: "#ab8bff",
+        light: {
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          300: "#D1D5DB",
+        },
+        dark: {
+          100: "#111827",
+          200: "#1F2937",
+          300: "#374151",
+        },
+        secondary: "#FBBF24",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
